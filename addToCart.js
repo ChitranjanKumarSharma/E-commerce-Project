536e4b25b1dc9ffba3066d031511884f587fea9f
ChitranjanKumarSharma <sharmacks822@gmail.com>
2024-07-03 16:53:30 +0530

    initial commit

---
+++ addToCart.js
@@ -0,0 +1,7 @@
+export const addToCart = ( event, id, stock)=>{
+    
+    const currentProdElem = document.querySelector(`#card${id}`);
+
+    console.log(currentProdElem);
+};
+
---
+++ main.js
@@ -0,0 +1,6 @@
+import products from "./api/products.json";
+import { showProductContainer } from "./homeProductCards";
+
+
+/* call the function which will take products array as input */
+showProductContainer(products);
